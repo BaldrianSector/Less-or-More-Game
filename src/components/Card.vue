6fc6ede607b4b00d8fec6c345133f093f75e24ef
Baldrian Sector <baldriansector@gmail.com>
2024-05-02 21:26:29 +0200

    Update Card styling

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -6,14 +6,14 @@ export default {
 
 <template>
 
-    <div class="transform transition duration-200 hover:scale-125  rounded-lg shadow-lg h-72 w-56 hover:shadow-xl bg-white text-black">
+    <div class="transform transition duration-200 hover:scale-125  rounded-lg shadow-lg h-80 w-64 hover:shadow-xl bg-white text-black cursor-pointer">
 
         <div :style="{ backgroundImage: 'url(' + image + ')' }" class="bg-cover bg-center m-2 h-3/6 rounded-lg"></div>
 
         <div class="px-5 pt-2 flex flex-col">
             <div class="flex justify-between">
-                <h2 class="font-semibold ">{{ country }}</h2>
-                <h2 class="font-semibold select-none">{{ flag }}</h2>
+                <h2>{{ country }}</h2>
+                <p class="font-semibold select-none">{{ flag }}</p>
             </div>
             <p class="text-sm text-gray-500 mt-3">{{ info }}</p>
         </div>
